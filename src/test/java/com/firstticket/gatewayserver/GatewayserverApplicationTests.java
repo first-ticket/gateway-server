310a5be28b00e6c6a950b6d8fe3668f271800e31
Dong Jin Park <160587239+straycat405@users.noreply.github.com>
2026-05-17 02:12:17 +0900

    [chore][gateway-server] CI/CD ECS 배포 워크플로우 추가 (#15)
    * chore: gateway CI/CD ECS 배포 이미지 미갱신 버그 수정
    
    - ECR에 푸시한 이미지 SHA가 ECS task definition에 반영되지 않던 문제 수정
    - amazon-ecs-render-task-definition 스텝 추가로 image URI 갱신
    - jq 오버라이드 입력을 render 출력 경로로 변경하여 처리 순서 보정
    
    * - 테스트 코드 수정

--- src/test/java/com/firstticket/gatewayserver/GatewayserverApplicationTests.java
+++ src/test/java/com/firstticket/gatewayserver/GatewayserverApplicationTests.java
@@ -2,12 +2,22 @@ package com.firstticket.gatewayserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+	"spring.cloud.config.enabled=false",
+	"eureka.client.enabled=false",
+	"spring.data.redis.host=localhost"
+})
 class GatewayserverApplicationTests {
+
+	@MockitoBean
+	ReactiveJwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
 	}
-
 }
